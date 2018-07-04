85e50dd3a370e229132db5e6497e1b575a0d2aa7
DESKTOP-FALGG2T\Karol Sobanski <sobanskikarol91@gmail.com>
2018-07-05 00:12:58 +0200

    Project

---
+++ Duel/Assets/Player.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    List<Card> cards = new List<Card>();
+    List<Wonder> wonders= new List<Wonder>();
+    int money;
+    
+}
