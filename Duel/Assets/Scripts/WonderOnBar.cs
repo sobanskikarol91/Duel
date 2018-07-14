a19854286f12851d3b8215715cfced4aad8199fa
DESKTOP-FALGG2T\Karol Sobanski <sobanskikarol91@gmail.com>
2018-07-15 00:06:24 +0200

    Clear Project from rubbish

---
+++ Duel/Assets/Scripts/WonderOnBar.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class WonderOnBar : MonoBehaviour
+{
+    Image _image;
+    Wonder _Wonder;
+
+    private void Awake()
+    {
+        _image = GetComponent<Image>();
+    }
+
+    public void SetWonder(Wonder w)
+    {
+        _Wonder = w;
+        _image.sprite = w.sprite;
+    }
+}
