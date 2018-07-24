5e6b5575e75a90f0026f7074013363cf99f08ad1
DESKTOP-FALGG2T\Karol Sobanski <sobanskikarol91@gmail.com>
2018-07-24 21:44:50 +0200

    Snd for Pawn

--- Duel/Assets/Scripts/ConflictPawn.cs
+++ Duel/Assets/Scripts/ConflictPawn.cs
@@ -11,5 +11,6 @@ public class ConflictPawn : MonoBehaviour
     {
         StandingPoint = value;
         transform.localPosition += value * offset;
+        GetComponent<AudioSource>().Play();
     }
 }
