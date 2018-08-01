2ded90d852e36ef8a1ff604be486dc495009df6b
DESKTOP-FALGG2T\Karol Sobanski <sobanskikarol91@gmail.com>
2018-08-01 22:36:21 +0200

    Open and closs DiscardedCardsWindow after click on discardedDeck on Table

--- Duel/Assets/DiscardedCardsUI.cs
+++ Duel/Assets/DiscardedCardsUI.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 
 public class DiscardedCardsUI : Singleton<DiscardedCardsUI>
 {
+    [SerializeField] GameObject discardCardWindow;
     [SerializeField] RectTransform cardsHolder;
     [SerializeField] GameObject discardedCardPrefab;
 
@@ -16,14 +17,13 @@ public class DiscardedCardsUI : Singleton<DiscardedCardsUI>
         cardGO.transform.localScale = Vector3.one;
         cardGO.transform.rotation = Camera.main.transform.rotation;
         cardGO.transform.localPosition = Vector3.zero;
+        discardCardWindow.SetActive(false);
     }
 
     public void ShowCardsOnScreen()
     {
-        List<Card> _discardedCards = DeckManager.instance.GetDiscardedCards();
-        _discardedCards.ForEach(c => Debug.Log(c.name));
-
-        int nr = 0;
-        //_discardedCards.ForEach(d => images[nr++].sprite = d.cardImg);
+        Debug.Log("a");
+        bool state = discardCardWindow.gameObject.active ? false : true;
+        discardCardWindow.gameObject.SetActive(state);
     }
 }
