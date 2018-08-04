93ef759189a66acb58ee810080c1e91361cb1650
DESKTOP-FALGG2T\Karol Sobanski <sobanskikarol91@gmail.com>
2018-08-04 19:16:52 +0200

    Price: Added +/- operators and constructor

--- Duel/Assets/Scripts/Price.cs
+++ Duel/Assets/Scripts/Price.cs
@@ -2,13 +2,6 @@
 using System.Collections;
 using System.Collections.Generic;
 
-//[System.Serializable]
-//public class Resource
-//{
-//    public PRODUCE type;
-//    public int value;
-//}
-
 [System.Serializable]
 public class Price
 {
@@ -18,6 +11,16 @@ public class Price
     public int glass;
     public int papyrus;
     public int gold;
+
+    public Price(int wood, int brick, int rock, int glass, int papyrus, int gold)
+    {
+        this.wood = wood;
+        this.brick = brick;
+        this.rock = rock;
+        this.glass = glass;
+        this.papyrus = papyrus;
+        this.gold = gold;
+    }
 
     public static bool operator >=(Price p1, Price p2)
     {
@@ -37,5 +40,29 @@ public class Price
                p1.glass <= p2.glass &&
                p1.papyrus <= p2.papyrus &&
                p1.gold <= p2.gold;
+    }
+
+    public static Price operator -(Price p1, Price p2)
+    {
+        int wood = p1.wood - p2.wood;
+        int brick = p1.brick - p2.brick;
+        int rock = p1.rock - p2.rock;
+        int glass = p1.glass - p2.glass;
+        int papyrus = p1.papyrus - p2.papyrus;
+        int gold = p1.gold - p2.gold;
+
+        return new Price(wood, brick, rock, glass, papyrus, gold);
+    }
+
+    public static Price operator +(Price p1, Price p2)
+    {
+        int wood = p1.wood + p2.wood;
+        int brick = p1.brick + p2.brick;
+        int rock = p1.rock + p2.rock;
+        int glass = p1.glass + p2.glass;
+        int papyrus = p1.papyrus + p2.papyrus;
+        int gold = p1.gold + p2.gold;
+
+        return new Price(wood, brick, rock, glass, papyrus, gold);
     }
 }
